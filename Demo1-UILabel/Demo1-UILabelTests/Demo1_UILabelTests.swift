fd5c6496d0043afd738272b616734b83589711fb
PrashantKumar Mangukiya <pbMangukiya@gmail.com>
2015-09-24 18:06:36 +0530

    Source Add: Demo1-UILabel  (Swift 2, Xcode 7, iOS 9)
    Added source code for UILabel usage demo. (Build using Swift2, Xcode 7,
    iOS 9)

---
+++ Demo1-UILabel/Demo1-UILabelTests/Demo1_UILabelTests.swift
@@ -0,0 +1,36 @@
+//
+//  Demo1_UILabelTests.swift
+//  Demo1-UILabelTests
+//
+//  Created by Prashant on 24/09/15.
+//  Copyright © 2015 PrashantKumar Mangukiya. All rights reserved.
+//
+
+import XCTest
+@testable import Demo1_UILabel
+
+class Demo1_UILabelTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
